4b658a36e752e21be397afb1748bfdd0c61d0e46
Perneni <nageswararao.perneni@accenture.com>
2025-05-05 15:19:50 +0530

    added

---
+++ uc3_project/notebooks/01-Data-Preparation-and-Index.py
@@ -0,0 +1,311 @@
+# Databricks notebook source
+# MAGIC %md 
+# MAGIC *Note: RAG performs document searches using Databricks Vector Search. In this notebook, we assume that the search index is ready for use. Make sure you run the previous [01-Data-Preparation-and-Index]($./01-Data-Preparation-and-Index [DO NOT EDIT]) notebook.*
+# MAGIC
+
+# COMMAND ----------
+
+# DBTITLE 1,Install the required libraries
+# MAGIC %pip install --quiet -U pydantic==2.9.2 databricks-sdk==0.40.0 databricks-langchain databricks-agents mlflow[databricks] langchain==0.3.19 langchain_core==0.3.37 databricks-vectorsearch==0.49 
+
+# COMMAND ----------
+
+#%pip install -qU databricks-langchain
+#%pip install -U mlflow
+dbutils.library.restartPython()
+
+# COMMAND ----------
+
+# MAGIC %run ../_resources/00-init $reset_all_data=false
+
+# COMMAND ----------
+
+# MAGIC %md-sandbox
+# MAGIC ### Building our Chain
+# MAGIC
+# MAGIC In this example, we'll assume you already have a basic understanding of langchain. Check our [previous notebook]($../00-first-step/01-First-Step-RAG-On-Databricks) to take it one step at a time!
+
+# COMMAND ----------
+
+VECTOR_SEARCH_ENDPOINT_NAME
+
+# COMMAND ----------
+
+rag_chain_config = {
+    "databricks_resources": {
+        "llm_endpoint_name": "databricks-meta-llama-3-1-70b-instruct",
+        "vector_search_endpoint_name": VECTOR_SEARCH_ENDPOINT_NAME,
+    },
+    "input_example": {
+        "messages": [{"content": "What is Apache Spark?", "role": "user"}]
+    },
+    "llm_config": {
+        "llm_parameters": {"max_tokens": 1500, "temperature": 0.01},
+        "llm_prompt_template": "You are a trusted AI assistant that helps answer questions based only on the provided information. If you do not know the answer to a question, you truthfully say you do not know. Here is the history of the current conversation you are having with your user: {chat_history}. And here is some context which may or may not help you answer the following question: {context}.  Answer directly, do not repeat the question, do not start with something like: the answer to the question, do not add AI in front of your answer, do not say: here is the answer, do not mention the context or the question. Based on this context, answer this question: {question}",
+        "llm_prompt_template_variables": ["context", "chat_history", "question"],
+    },
+    "retriever_config": {
+        "chunk_template": "Passage: {chunk_text}\n",
+        "data_pipeline_tag": "poc",
+        "parameters": {"k": 5, "query_type": "ann"},
+        "schema": {"chunk_text": "content", "document_uri": "url", "primary_key": "id"},
+        "vector_search_index": f"{catalog}.{db}.databricks_documentation_vs_index",
+    },
+}
+try:
+    with open('rag_chain_config.yaml', 'w') as f:
+        yaml.dump(rag_chain_config, f)
+except:
+    print('pass to work on build job')
+model_config = mlflow.models.ModelConfig(development_config='rag_chain_config.yaml')
+
+# COMMAND ----------
+
+databricks_resources.get("vector_search_endpoint_name")
+
+# COMMAND ----------
+
+# DBTITLE 1,Write the chain to a companion file to avoid serialization issues
+# MAGIC %%writefile chain.py
+# MAGIC import os
+# MAGIC import mlflow
+# MAGIC from operator import itemgetter
+# MAGIC #from databricks_langchain import ChatDatabricks
+# MAGIC from databricks_langchain.chat_models import ChatDatabricks
+# MAGIC from databricks_langchain.vectorstores import DatabricksVectorSearch
+# MAGIC from langchain_core.runnables import RunnableLambda
+# MAGIC from langchain_core.output_parsers import StrOutputParser
+# MAGIC from langchain_core.prompts import PromptTemplate
+# MAGIC from langchain_core.runnables import RunnablePassthrough
+# MAGIC
+# MAGIC ## Enable MLflow Tracing
+# MAGIC mlflow.langchain.autolog()
+# MAGIC
+# MAGIC # Return the string contents of the most recent message from the user
+# MAGIC def extract_user_query_string(chat_messages_array):
+# MAGIC     return chat_messages_array[-1]["content"]
+# MAGIC
+# MAGIC def extract_previous_messages(chat_messages_array):
+# MAGIC     messages = "\n"
+# MAGIC     for msg in chat_messages_array[:-1]:
+# MAGIC         messages += (msg["role"] + ": " + msg["content"] + "\n")
+# MAGIC     return messages
+# MAGIC
+# MAGIC def combine_all_messages_for_vector_search(chat_messages_array):
+# MAGIC     return extract_previous_messages(chat_messages_array) + extract_user_query_string(chat_messages_array)
+# MAGIC
+# MAGIC #Get the conf from the local conf file
+# MAGIC model_config = mlflow.models.ModelConfig(development_config='rag_chain_config.yaml')
+# MAGIC
+# MAGIC databricks_resources = model_config.get("databricks_resources")
+# MAGIC retriever_config = model_config.get("retriever_config")
+# MAGIC llm_config = model_config.get("llm_config")
+# MAGIC
+# MAGIC vector_search_schema = retriever_config.get("schema")
+# MAGIC
+# MAGIC # Turn the Vector Search index into a LangChain retriever
+# MAGIC endpoint_name = databricks_resources.get("vector_search_endpoint_name")
+# MAGIC #endpoint_name ="databricks-meta-llama-3-3-70b-instruct"
+# MAGIC vector_search_as_retriever = DatabricksVectorSearch(
+# MAGIC     endpoint=endpoint_name,
+# MAGIC     index_name=retriever_config.get("vector_search_index")+"_motheesh",
+# MAGIC     columns=[
+# MAGIC         vector_search_schema.get("primary_key"),
+# MAGIC         vector_search_schema.get("chunk_text"),
+# MAGIC         vector_search_schema.get("document_uri"),
+# MAGIC     ],
+# MAGIC ).as_retriever(search_kwargs=retriever_config.get("parameters"))
+# MAGIC
+# MAGIC # Required to:
+# MAGIC # 1. Enable the RAG Studio Review App to properly display retrieved chunks
+# MAGIC # 2. Enable evaluation suite to measure the retriever
+# MAGIC mlflow.models.set_retriever_schema(
+# MAGIC     primary_key=vector_search_schema.get("primary_key"),
+# MAGIC     text_column=vector_search_schema.get("chunk_text"),
+# MAGIC     doc_uri=vector_search_schema.get("document_uri")
+# MAGIC )
+# MAGIC
+# MAGIC # Method to format the docs returned by the retriever into the prompt
+# MAGIC def format_context(docs):
+# MAGIC     chunk_template = retriever_config.get("chunk_template")
+# MAGIC     chunk_contents = [
+# MAGIC         chunk_template.format(
+# MAGIC             chunk_text=d.page_content,
+# MAGIC         )
+# MAGIC         for d in docs
+# MAGIC     ]
+# MAGIC     return "".join(chunk_contents)
+# MAGIC
+# MAGIC # Prompt Template for generation
+# MAGIC prompt = PromptTemplate(
+# MAGIC     template=llm_config.get("llm_prompt_template"),
+# MAGIC     input_variables=llm_config.get("llm_prompt_template_variables"),
+# MAGIC )
+# MAGIC
+# MAGIC # FM for generation
+# MAGIC model = ChatDatabricks(
+# MAGIC     endpoint=databricks_resources.get("llm_endpoint_name"),
+# MAGIC     extra_params=llm_config.get("llm_parameters"),
+# MAGIC )
+# MAGIC
+# MAGIC # RAG Chain
+# MAGIC chain = (
+# MAGIC     {
+# MAGIC         "question": itemgetter("messages") | RunnableLambda(extract_user_query_string),
+# MAGIC         "context": itemgetter("messages")
+# MAGIC         | RunnableLambda(combine_all_messages_for_vector_search)
+# MAGIC         | vector_search_as_retriever
+# MAGIC         | RunnableLambda(format_context),
+# MAGIC         "chat_history": itemgetter("messages") | RunnableLambda(extract_previous_messages)
+# MAGIC     }
+# MAGIC     | prompt
+# MAGIC     | model
+# MAGIC     | StrOutputParser()
+# MAGIC )
+# MAGIC
+# MAGIC # Tell MLflow logging where to find your chain.
+# MAGIC mlflow.models.set_model(model=chain)
+# MAGIC
+# MAGIC # COMMAND ----------
+# MAGIC
+# MAGIC # chain.invoke(model_config.get("input_example"))
+
+# COMMAND ----------
+
+# MAGIC %pip install pydantic==2.9.2
+
+# COMMAND ----------
+
+model_config.get("databricks_resources").get("llm_endpoint_name")
+
+# COMMAND ----------
+
+from mlflow.models.resources import DatabricksVectorSearchIndex, DatabricksServingEndpoint
+import mlflow
+# Log the model to MLflow
+endpoint_name=model_config.get("databricks_resources").get("llm_endpoint_name")
+#endpoint_name="databricks-meta-llama-3-3-70b-instruct"
+
+with mlflow.start_run(run_name="dbdemos_rag_quickstart"):
+    logged_chain_info = mlflow.langchain.log_model(
+        lc_model=os.path.join(os.getcwd(), 'chain.py'),  # Chain code file e.g., /path/to/the/chain.py 
+        model_config='rag_chain_config.yaml',  # Chain configuration 
+        artifact_path="chain",  # Required by MLflow
+        input_example=model_config.get("input_example"),  # Save the chain's input schema
+        resources=[
+            DatabricksVectorSearchIndex(index_name=model_config.get("retriever_config").get("vector_search_index")+"_motheesh"),
+            DatabricksServingEndpoint(endpoint_name=endpoint_name)
+        ]
+    )
+
+# Test the chain locally
+chain = mlflow.langchain.load_model(logged_chain_info.model_uri)
+chain.invoke(model_config.get("input_example"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Let's deploy our RAG application and open it for external expert users
+
+# COMMAND ----------
+
+from databricks import agents
+MODEL_NAME = "dbdemos_rag_demo"
+MODEL_NAME_FQN = f"{catalog}.{db}.{MODEL_NAME}"
+
+# COMMAND ----------
+
+instructions_to_reviewer = f"""### Instructions for Testing the our Databricks Documentation Chatbot assistant
+
+Your inputs are invaluable for the development team. By providing detailed feedback and corrections, you help us fix issues and improve the overall quality of the application. We rely on your expertise to identify any gaps or areas needing enhancement.
+
+1. **Variety of Questions**:
+   - Please try a wide range of questions that you anticipate the end users of the application will ask. This helps us ensure the application can handle the expected queries effectively.
+
+2. **Feedback on Answers**:
+   - After asking each question, use the feedback widgets provided to review the answer given by the application.
+   - If you think the answer is incorrect or could be improved, please use "Edit Answer" to correct it. Your corrections will enable our team to refine the application's accuracy.
+
+3. **Review of Returned Documents**:
+   - Carefully review each document that the system returns in response to your question.
+   - Use the thumbs up/down feature to indicate whether the document was relevant to the question asked. A thumbs up signifies relevance, while a thumbs down indicates the document was not useful.
+
+Thank you for your time and effort in testing our assistant. Your contributions are essential to delivering a high-quality product to our end users."""
+
+# Register the chain to UC
+uc_registered_model_info = mlflow.register_model(model_uri=logged_chain_info.model_uri, name=MODEL_NAME_FQN)
+
+# Deploy to enable the Review APP and create an API endpoint
+deployment_info = agents.deploy(model_name=MODEL_NAME_FQN, model_version=uc_registered_model_info.version, scale_to_zero=True)
+
+# Add the user-facing instructions to the Review App
+agents.set_review_instructions(MODEL_NAME_FQN, instructions_to_reviewer)
+
+wait_for_model_serving_endpoint_to_be_ready(deployment_info.endpoint_name)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Grant stakeholders access to the Mosaic AI Agent Evaluation App
+# MAGIC
+# MAGIC Now, grant your stakeholders permissions to use the Review App. To simplify access, stakeholders do not require to have Databricks accounts.
+
+# COMMAND ----------
+
+user_list = ["quentin.ambard@ford.com"]
+# Set the permissions.
+agents.set_permissions(model_name=MODEL_NAME_FQN, users=user_list, permission_level=agents.PermissionLevel.CAN_QUERY)
+
+print(f"Share this URL with your stakeholders: {deployment_info.review_app_url}")
+
+# COMMAND ----------
+
+# MAGIC %md ## Find review app name
+# MAGIC
+# MAGIC If you lose this notebook's state and need to find the URL to your Review App, you can list the chatbot deployed:
+
+# COMMAND ----------
+
+from databricks import agents
+MODEL_NAME = "dbdemos_rag_demo"
+MODEL_NAME_FQN = f"{catalog}.{db}.{MODEL_NAME}"
+for deployment in agents.list_deployments():
+  if deployment.model_name == MODEL_NAME_FQN:
+    print(f"Review App URL: {deployment.review_app_url}")    
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Congratulations! You have deployed your first GenAI RAG model!
+# MAGIC
+# MAGIC You're now ready to deploy the same logic for your internal knowledge base leveraging Lakehouse AI.
+# MAGIC
+# MAGIC We've seen how the Lakehouse AI is uniquely positioned to help you solve your GenAI challenge:
+# MAGIC
+# MAGIC - Simplify Data Ingestion and preparation with Databricks Engineering Capabilities
+# MAGIC - Accelerate Vector Search  deployment with fully managed indexes
+# MAGIC - Leverage Databricks DBRX Instruct foundation model endpoint
+# MAGIC - Deploy realtime model endpoint to perform RAG and provide Q&A capabilities
+# MAGIC
+# MAGIC Lakehouse AI is uniquely positioned to accelerate your GenAI deployment.
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Next: Deploying our GenAI Assistant application to end users with Databricks Lakehouse Application
+# MAGIC
+# MAGIC We are now ready to build a front end application so that our users can ask questions to the chatbot. 
+# MAGIC
+# MAGIC Open the [03-Deploy-Frontend-Lakehouse-App]($./03-Deploy-Frontend-Lakehouse-App) how to deploy your first Lakehouse Application.
+
+# COMMAND ----------
+
+# MAGIC %md # Cleanup
+# MAGIC
+# MAGIC To free up resources, please delete uncomment and run the below cell.
+
+# COMMAND ----------
+
+# /!\ THIS WILL DROP YOUR DEMO SCHEMA ENTIRELY /!\ 
+# cleanup_demo(catalog, db, serving_endpoint_name, f"{catalog}.{db}.databricks_documentation_vs_index")
---
+++ uc3_project/notebooks/02-Deploy-RAG-Chatbot-Model.py
@@ -0,0 +1,309 @@
+# Databricks notebook source
+# DBTITLE 1,Install required external libraries
+# MAGIC %pip install --quiet -U mlflow[databricks] lxml==4.9.3 transformers==4.49.0 langchain==0.3.19 databricks-vectorsearch==0.49 bs4==0.0.2 markdownify==0.14.1
+# MAGIC dbutils.library.restartPython()
+
+# COMMAND ----------
+
+!python3 --version
+
+# COMMAND ----------
+
+# DBTITLE 1,Init our resources and catalog
+# MAGIC %run ../_resources/00-init $reset_all_data=false
+
+# COMMAND ----------
+
+import markdownify
+from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ET
+import requests
+# Fetch the XML content from sitemap
+response = requests.get(DATABRICKS_SITEMAP_URL)
+root = ET.fromstring(response.content)
+max_documents=None
+# Find all 'loc' elements (URLs) in the XML
+urls = [loc.text for loc in root.findall(".//{http://www.sitemaps.org/schemas/sitemap/0.9}loc")]
+if max_documents:
+  urls = urls[:max_documents]
+
+    # Create DataFrame from URLs
+df_urls = spark.createDataFrame(urls, StringType()).toDF("url").repartition(10)
+df_urls = df_urls.filter(df_urls.url.startswith("https://www.ford.com/finance/customer-support"))
+#df_urls = df_urls.limit(1)
+
+    # Pandas UDF to fetch HTML content for a batch of URLs
+@pandas_udf("string")
+def fetch_html_udf(urls: pd.Series) -> pd.Series:
+  adapter = HTTPAdapter(max_retries=retries)
+  http = requests.Session()
+  http.mount("http://", adapter)
+  http.mount("https://", adapter)
+  def fetch_html(url):
+    try:
+      response = http.get(url)
+      if response.status_code == 200:
+        return response.content
+    except requests.RequestException:
+      return None
+    return None
+
+  with ThreadPoolExecutor(max_workers=200) as executor:
+    results = list(executor.map(fetch_html, urls))
+    return pd.Series(results)
+
+    # Pandas UDF to process HTML content and extract text
+@pandas_udf("string")
+def download_web_page_udf(html_contents: pd.Series) -> pd.Series:
+  def extract_text(html_content):
+    if html_content:
+      soup = BeautifulSoup(html_content, "html.parser")
+      article = soup.find("div", {"class": "astute-onetopic-result-n"})
+      if article:
+        try:
+          return markdownify.markdownify(article.prettify(), heading_style="ATX")
+        except Exception as e:
+          return None
+    return None
+
+  return html_contents.apply(extract_text)
+
+
+
+# COMMAND ----------
+
+    # Apply UDFs to DataFrame
+df_with_html = df_urls.withColumn("html_content", fetch_html_udf("url"))
+display(df_with_html)
+
+
+# COMMAND ----------
+
+final_df = df_with_html.withColumn("text", download_web_page_udf("html_content"))
+display(final_df)
+
+# COMMAND ----------
+
+display(spark.table("databricks_documentation_vs_index_motheesh").limit(2))
+
+# COMMAND ----------
+
+if not spark.catalog.tableExists("ford_documentation") or spark.table("ford_documentation").isEmpty():
+    # Download Databricks documentation to a DataFrame (see _resources/00-init for more details)
+    doc_articles = download_databricks_documentation_articles()
+    #Save them as a raw_documentation table
+    doc_articles.write.mode('overwrite').saveAsTable("ford_documentation")
+
+display(spark.table("ford_documentation").limit(2))
+
+# COMMAND ----------
+
+# DBTITLE 1,Splitting our html pages in smaller chunks
+import re
+from langchain.text_splitter import MarkdownHeaderTextSplitter, RecursiveCharacterTextSplitter
+from transformers import AutoTokenizer, OpenAIGPTTokenizer
+
+max_chunk_size = 500
+
+tokenizer = OpenAIGPTTokenizer.from_pretrained("openai-gpt")
+text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(tokenizer, chunk_size=max_chunk_size, chunk_overlap=50)
+md_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=[("##", "header2")])
+
+# Split on H2, but merge small h2 chunks together to avoid having too small chunks. 
+def split_html_on_h2(html, min_chunk_size=20, max_chunk_size=500):
+    if not html:
+        return []
+    #removes b64 images captured in the md    
+    html = re.sub(r'data:image\/[a-zA-Z]+;base64,[A-Za-z0-9+/=\n]+', '', html, flags=re.MULTILINE)
+    chunks = []
+    previous_chunk = ""
+    for c in md_splitter.split_text(html):
+        content = c.metadata.get('header2', "") + "\n" + c.page_content
+        if len(tokenizer.encode(previous_chunk + content)) <= max_chunk_size / 2:
+            previous_chunk += content + "\n"
+        else:
+            chunks.extend(text_splitter.split_text(previous_chunk.strip()))
+            previous_chunk = content + "\n"
+    if previous_chunk:
+        chunks.extend(text_splitter.split_text(previous_chunk.strip()))
+    return [c for c in chunks if len(tokenizer.encode(c)) > min_chunk_size]
+
+# Let's try our chunking function
+html = spark.table("ford_documentation").limit(1).collect()[0]['text']
+split_html_on_h2(html)
+
+# COMMAND ----------
+
+# DBTITLE 1,Create the final databricks_documentation table containing chunks
+# MAGIC %sql
+# MAGIC --Note that we need to enable Change Data Feed on the table to create the index
+# MAGIC CREATE TABLE IF NOT EXISTS databricks_documentation (
+# MAGIC   id BIGINT GENERATED BY DEFAULT AS IDENTITY,
+# MAGIC   url STRING,
+# MAGIC   content STRING,
+# MAGIC   embeddings STRING
+# MAGIC ) TBLPROPERTIES (delta.enableChangeDataFeed = true); 
+
+# COMMAND ----------
+
+# Let's create a user-defined function (UDF) to chunk all our documents with spark
+@pandas_udf("array<string>")
+def parse_and_split(docs: pd.Series) -> pd.Series:
+    return docs.apply(split_html_on_h2)
+    
+(spark.table("ford_documentation")
+      .filter('text is not null')
+      .repartition(30)
+      .withColumn('content', F.explode(parse_and_split('text')))
+      .drop("text")
+      .write.mode('overwrite').saveAsTable("databricks_documentation"))
+
+display(spark.table("databricks_documentation"))
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC --Note that we need to enable Change Data Feed on the table to create the index
+# MAGIC CREATE TABLE IF NOT EXISTS support_doc_embeddings_v2 (
+# MAGIC   id BIGINT GENERATED BY DEFAULT AS IDENTITY,
+# MAGIC   url STRING,
+# MAGIC   content STRING,
+# MAGIC   embeddings ARRAY<STRING>
+# MAGIC ) TBLPROPERTIES (delta.enableChangeDataFeed = true); 
+
+# COMMAND ----------
+
+import mlflow.deployments
+import array
+deploy_client = mlflow.deployments.get_deploy_client("databricks")
+
+def get_embeddings(input: pd.Series):
+    response = deploy_client.predict(endpoint="databricks-gte-large-en", inputs={"input": [input]})
+    embeddings = [e['embedding'] for e in response.data]
+    return input
+# Register the Python function as a UDF
+getEmbeddings = F.udf(
+    lambda content: content,#get_embeddings(content),
+    "array<double>",
+)
+docs=spark.table("databricks_documentation").withColumn("embeddings", getEmbeddings("content"))
+
+
+# COMMAND ----------
+
+import array
+
+
+
+def get_embeddings(input):
+    response = deploy_client.predict(endpoint="databricks-gte-large-en", inputs={"input": [str(input)]})
+    embeddings = [str(e['embedding']) for e in response.data]
+    return embeddings
+
+@pandas_udf("array<string>")
+def test(input: pd.Series) -> pd.Series:
+    result=input.apply(get_embeddings)
+    return result
+
+docs=spark.table("databricks_documentation").withColumn("embeddings", test("content"))
+docs.collect()
+
+# COMMAND ----------
+
+docs.write.mode('overwrite').saveAsTable("support_doc_embeddings_v2")
+
+# COMMAND ----------
+
+# DBTITLE 1,What is an embedding
+import mlflow.deployments
+deploy_client = mlflow.deployments.get_deploy_client("databricks")
+
+#Embeddings endpoints convert text into a vector (array of float). Here is an example using GTEgte:
+response = deploy_client.predict(endpoint="databricks-gte-large-en", inputs={"input": ["What is Apache Spark?"]})
+embeddings = [e['embedding'] for e in response.data]
+print(embeddings)
+#
+
+# COMMAND ----------
+
+# DBTITLE 1,Creating the Vector Search endpoint
+from databricks.vector_search.client import VectorSearchClient
+vsc = VectorSearchClient()
+if not endpoint_exists(vsc, VECTOR_SEARCH_ENDPOINT_NAME):
+    vsc.create_endpoint(name=VECTOR_SEARCH_ENDPOINT_NAME, endpoint_type="STANDARD")
+
+wait_for_vs_endpoint_to_be_ready(vsc, VECTOR_SEARCH_ENDPOINT_NAME)
+print(f"Endpoint named {VECTOR_SEARCH_ENDPOINT_NAME} is ready.")
+
+# COMMAND ----------
+
+# DBTITLE 1,Create the managed vector search using our endpoint
+from databricks.sdk import WorkspaceClient
+import databricks.sdk.service.catalog as c
+
+#The table we'd like to index
+source_table_fullname = f"{catalog}.{db}.databricks_documentation"
+# Where we want to store our index
+vs_index_fullname = f"{catalog}.{db}.databricks_documentation_vs_index_motheesh"
+
+if not index_exists(vsc, VECTOR_SEARCH_ENDPOINT_NAME, vs_index_fullname):
+  print(f"Creating index {vs_index_fullname} on endpoint {VECTOR_SEARCH_ENDPOINT_NAME}...")
+  try:
+    vsc.create_delta_sync_index(
+      endpoint_name=VECTOR_SEARCH_ENDPOINT_NAME,
+      index_name=vs_index_fullname,
+      source_table_name=source_table_fullname,
+      pipeline_type="TRIGGERED",
+      primary_key="id",
+      embedding_source_column='content', #The column containing our text
+      embedding_model_endpoint_name='databricks-gte-large-en' #The embedding endpoint used to create the embeddings
+    )
+  except Exception as e:
+    display_quota_error(e, VECTOR_SEARCH_ENDPOINT_NAME)
+    raise e
+  #Let's wait for the index to be ready and all our embeddings to be created and indexed
+  wait_for_index_to_be_ready(vsc, VECTOR_SEARCH_ENDPOINT_NAME, vs_index_fullname)
+else:
+  #Trigger a sync to update our vs content with the new data saved in the table
+  wait_for_index_to_be_ready(vsc, VECTOR_SEARCH_ENDPOINT_NAME, vs_index_fullname)
+  vsc.get_index(VECTOR_SEARCH_ENDPOINT_NAME, vs_index_fullname).sync()
+
+print(f"index {vs_index_fullname} on table {source_table_fullname} is ready")
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC ## Searching for similar content
+# MAGIC
+# MAGIC That's all we have to do. Databricks will automatically capture and synchronize new entries in your Delta Live Table.
+# MAGIC
+# MAGIC Note that depending on your dataset size and model size, index creation can take a few seconds to start and index your embeddings.
+# MAGIC
+# MAGIC Let's give it a try and search for similar content.
+# MAGIC
+# MAGIC *Note: `similarity_search` also support a filters parameter. This is useful to add a security layer to your RAG system: you can filter out some sensitive content based on who is doing the call (for example filter on a specific department based on the user preference).*
+
+# COMMAND ----------
+
+import mlflow.deployments
+deploy_client = mlflow.deployments.get_deploy_client("databricks")
+
+question = "How can I track billing usage on my workspaces?"
+
+results = vsc.get_index(VECTOR_SEARCH_ENDPOINT_NAME, vs_index_fullname).similarity_search(
+  query_text=question,
+  columns=["url", "content"],
+  num_results=1)
+docs = results.get('result', {}).get('data_array', [])
+docs
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC ## Next step: Deploy our chatbot model with RAG using DBRX
+# MAGIC
+# MAGIC We've seen how Databricks Lakehouse AI makes it easy to ingest and prepare your documents, and deploy a Vector Search index on top of it with just a few lines of code and configuration.
+# MAGIC
+# MAGIC This simplifies and accelerates your data projects so that you can focus on the next step: creating your real-time chatbot endpoint with well-crafted prompt augmentation.
+# MAGIC
+# MAGIC Open the [02-Deploy-RAG-Chatbot-Model]($./02-Deploy-RAG-Chatbot-Model) notebook to create and deploy a chatbot endpoint.
